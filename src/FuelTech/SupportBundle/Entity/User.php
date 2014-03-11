0e8dc85de006a8989a130a772724a1cd2d449798
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-03-11 14:33:47 +0100

    FOSUserbundle installed

---
+++ src/FuelTech/SupportBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FuelTech\SupportBundle\Entity;
+
+use FOS\UserBundle\Model\User as FOS_User;
+
+/**
+ * Description of User
+ *
+ * @author cyber02
+ */
+class User extends FOS_User
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
